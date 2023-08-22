b7d2f1804d28acb54892c09983b186da52b7546f
Marco-Thiele <125655717+Marco-Thiele@users.noreply.github.com>
2023-08-22 13:29:31 +0200

    a-href

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -3,10 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent {
-images = ['0.png', '1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png'];
-imagesName = ['Angular', 'TypeScript', 'JavaScript', 'HTML', 'Firebase', 'Git', 'CSS', 'Rest-Api', 'Scrum', 'Material design']
+  images = ['0.png', '1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png'];
+  imagesName = ['Angular', 'TypeScript', 'JavaScript', 'HTML', 'Firebase', 'Git', 'CSS', 'Rest-Api', 'Scrum', 'Material design'];
+
+
+  scrollToSection(sectionId) {
+    const targetSection = document.getElementById(sectionId);
+    if (targetSection) {
+      const offset = -150;
+      const targetPosition = targetSection.offsetTop + offset;
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
+
 
 }
--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class StartScreenComponent {
 
+  scrollToSection(sectionId: string): void{
+    const targetSection = document.getElementById(sectionId);
+    if (targetSection) {
+      const offset = -150;
+      const targetPosition = targetSection.offsetTop + offset;
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
+
 }
